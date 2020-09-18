d198b4bad67343a07985d31e3b0bcfa37753f161
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-18 16:51:40 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map( movie => {
+        return(
+          <div>
+          <h2>{movie.title}</h2>
+          <p>{movie.time}</p>
+          <ul>
+          {movie.genres.map(g => {
+            return(
+                <li>{g}</li>
+            )
+          })}
+          </ul>
+        </div>
+        )
+
+      } )}
     </div>
   );
 };
